98e19d108f2c6c22b7e14ac6d1313f6649220702
KARTHICK877 <kmass8754@gmail.com>
2023-11-25 18:41:51 +0530

    update

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/password-reset", passwordResetRoutes);
 app.use(formData.parse())
 app.use('/api', formRoutes);
-app.get("/api/welcome", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).send({ message: "Welcome to my sign-up page!" });
 });
 
